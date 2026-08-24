9e8644cfe43b65e9df322482d6b2eb970f6740b0
MalihaaRaza <maliharaza89@gmail.com>
2026-08-24 17:41:28 -0400

    fix indentation

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -18,12 +18,12 @@ class CarMake(models.Model):
 class CarModel(models.Model):
     # Many-To-One relationship to Car Make model
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
- 
+
     # Dealer ID refers to a dealer created in Cloudant database
     dealer_id = models.IntegerField(default=1)
- 
+
     name = models.CharField(max_length=100)
- 
+
     # Type choices: Sedan, SUV, Wagon, etc.
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -33,7 +33,7 @@ class CarModel(models.Model):
         ('TRUCK', 'Truck'),
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
- 
+
     # Year with min value 2015 and max value 2023
     year = models.IntegerField(
         default=2023,
@@ -42,9 +42,9 @@ class CarModel(models.Model):
             MinValueValidator(2015)
         ]
     )
- 
+
     # Additional useful field
     dealer_name = models.CharField(max_length=100, blank=True)
- 
+
     def __str__(self):
         return f"{self.car_make.name} {self.name}"  # Return car make and model
